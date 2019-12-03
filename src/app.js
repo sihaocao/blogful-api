50f13af2a172ac73ad376659f3aa8e27af13115d
scao <sihao.cao@gmail.com>
2019-12-03 11:13:37 -0500

    added POST and DELETE

--- src/app.js
+++ src/app.js
@@ -1,57 +1,34 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
-const ArticlesService = require('./articles-service');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const articlesRouter = require('./articles/articles-router')
 
-const app = express();
+const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
+app.use(cors())
+app.use(helmet())
 
-app.use(morgan(morganOption));
-app.use(helmet());
-app.use(cors());
-
-app.get('/articles', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getAllArticles(knexInstance)
-        .then(articles => {
-            res.json(articles)
-        })
-        .catch(next)
-})
-
-app.get('/articles/:article_id', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getById(knexInstance, req.params.article_id)
-        .then(article => {
-            if (!article) {
-                return res.status(404).json({
-                    error: { message: `Article doesn't exist` }
-                })
-            }
-            res.json(article)
-        })
-        .catch(next)
-})
+app.use('/articles', articlesRouter)
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+  res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
-    let response;
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error);
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
 })
 
-module.exports = app;
+module.exports = app
