cac0d8efdebe085cf8b2f0c744fb1b1bebe644d0
scao <sihao.cao@gmail.com>
2019-12-09 20:13:37 -0500

    removed log in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const knex = require('knex');
 const app = require('./app');
 const { PORT, DATABASE_URL } = require('./config');
-console.log('DATABASE_URL', DATABASE_URL)
+
 const db = knex({
     client: 'pg',
     connection: DATABASE_URL,
